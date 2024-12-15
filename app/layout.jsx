94050133273577e47c42e767951dedcb4c21ce33
Harikalyan <harikalyan99tronics@gmail.com>
2024-12-15 11:45:13 +0530

    added the meta

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,6 +7,10 @@ export const metadata = {
   title: "Tron's AI",
   description:
     "Integrating Tron's AI into your Next.js app opens the door to countless possibilities. From image and video analysis to real-time chatbots, Tron's AI allows you to rapidly build intelligent and interactive features that enhance user engagement and improve overall app performance.",
+  icons: {
+    icon: "/assets/images/logo.png",
+  },
+   
 };
 
 const RootLayout = ({ children }) => {
